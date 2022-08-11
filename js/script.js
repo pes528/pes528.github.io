daa6b5c6a186ac72de7d29c8ac30e7a71fcfe5e7
pes528 <103068790+pes528@users.noreply.github.com>
2022-08-10 21:52:00 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function dividir(num1, num2){
+    return (num1/num);
+
+
+
+}
+    
+function mek(){
+    var bod=document.getElementById("body")
+    var edad=document.getElementById("number").value;
+    var ed=2022-edad
+    var edd=document.getElementById("res")
+    var siu=document.getElementById("miki")
+    //alert("tu edad es: "+ed );
+    edd.value=ed;
+    
+    siu.innerHTML="dsa";
+    
+    
+};
