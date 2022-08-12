6b2bbcc41e22ba9bc7538b7b55d0ce4ca56f995a
pes528 <103068790+pes528@users.noreply.github.com>
2022-08-12 19:21:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-function dividir(num1, num2){
-    return (num1/num);
+simplyCountdown('#cont', {
+    year: 2022, // required
+    month: 09, // required
+    day: 20, // required
+    hours: 11, // Default is 0 [0-23] integer
+    minutes: 0, // Default is 0 [0-59] integer
+    seconds: 0, // Default is 0 [0-59] integer
+    words: { //words displayed into the countdown
+        days: { singular: 'day', plural: 'DAYS' },
+        hours: { singular: 'hour', plural: 'HOURS' },
+        minutes: { singular: 'minute', plural: 'MINUNTES' },
+        seconds: { singular: 'second', plural: 'SECONDS' }
+    },
+    plural: true, //use plurals
+    inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+    inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+    // in case of inline set to false
+    enableUtc: false,
+    onEnd: function () {
+        // your code
+        return;
+    },
+    refresh: 1000, //default refresh every 1s
+    sectionClass: 'simply-section', //section css class
+    amountClass: 'simply-amount', // amount css class
+    wordClass: 'simply-word', // word css class
+    zeroPad: false,
+    countUp: false, // enable count up if set to true
+});
 
+// Also, you can init with already existing Javascript Object.
+let myElement = document.querySelector('.my-countdown');
+simplyCountdown(myElement, { /* options */ });
 
-
-}
-    
-function mek(){
-    var bod=document.getElementById("body")
-    var edad=document.getElementById("number").value;
-    var ed=2022-edad
-    var edd=document.getElementById("res")
-    var siu=document.getElementById("miki")
-    //alert("tu edad es: "+ed );
-    edd.value=ed;
-    
-    siu.innerHTML="dsa";
-    
-    
-};
+let multipleElements = document.querySelectorAll('.my-countdown');
+simplyCountdown(multipleElements, { /* options */ });
